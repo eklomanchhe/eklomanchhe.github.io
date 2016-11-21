0a73821bafb748bb38db5cad243b4d9e71c6d1bd
Anil Shivakoti <cvakotianil@gmail.com>
2016-11-21 22:56:55 +0000

    Add files via upload

---
+++ eklomanchhe/contact.php
@@ -0,0 +1,71 @@
+<!-- PAGE : CONTACT -->
+        <section id="contact" class="page wrapper" data-x="14500" data-y="5250" data-rotate="90" data-scale="2">
+        	
+            <!-- PAGE TITLE -->
+        	<h2 class="caption"><span>CONTACT</span></h2>
+        
+            <div class="iscroll-wrapper">
+                <div class="scroller">
+                    
+                
+                    <!--row-->
+                    <div class="row">   
+                
+                
+                        
+                        <!--column 7/12-->
+                        <div class="map span7">
+                            
+                            <h3>Now-a-Days I'm in...</h3>
+                            
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2382.119250712306!2d-6.267902248702675!3d53.34112148270555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e9deac960b1%3A0x39edc5e26f399616!2s77+Aungier+St%2C+Dublin+2%2C+Ireland!5e0!3m2!1sen!2sie!4v1474651288041"></iframe>
+                            
+                        </div>
+                        <!--column 7/12-->
+                        
+                        
+                        
+                        <!--column 5/12-->
+                        <div class="span5">
+                            
+                            <h3>Want To send me message???</h3>
+                            
+                            <!--CONTACT FORM-->
+                        	<form id="contact-form" action="send-mail.php" method="post" class="validate-form">
+                            	<fieldset>
+                                
+                                <!-- personal info -->
+                                 <ul class="personal-info contact-form">
+                                    <li><label class="title" for="name">NAME</label><input class="text required" id="name" name="name" type="text" placeholder="Your name Please"></li>
+                                    <li><label class="title" for="email">E-MAIL</label><input class="text required email" id="email" name="email" type="text" placeholder="Valid email, So I can get back to You"></li>
+                                    <li><label class="title" for="message">MESSAGE</label><textarea class="text required" id="message" name="message" placeholder="Your Message To me"></textarea></li>
+                                    <li class="submit-area">
+                                        <button type="submit" class="btn-send">SEND T0 ME</button>
+                                        <input type="hidden" name="submitted" id="submitted" value="true" />
+                                        <span class="icon">Ã</span>
+                                    </li>
+                                 </ul>
+                                 <!-- personal info -->
+                             
+                                 </fieldset>
+                            </form> 
+                            <!--CONTACT FORM-->
+                        
+                        
+                        </div>
+                        <!--column 5/12-->
+                        
+                        
+                    </div>
+                    <!--row-->
+                    
+                    
+                    
+                </div>
+                <!--end scroller-->
+            </div>
+            <!--iscroll-wrapper-->
+        
+		
+        </section>
+        <!-- PAGE : CONTACT -->
