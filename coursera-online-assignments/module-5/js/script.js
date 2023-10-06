10ba23a6ac652fe9fd977969009fe588c20ce7ba
Anil Shivakoti <cvakotianil@gmail.com>
2023-10-06 10:31:43 +0545

    Update script.js

--- coursera-online-assignments/module-5/js/script.js
+++ coursera-online-assignments/module-5/js/script.js
@@ -15,11 +15,11 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "https://davids-restaurant.herokuapp.com/categories.json";
+  "http://eklomanchhe.github.io/coursera-online-assignments/module-5/index.html/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "http://eklomanchhe.github.io/coursera-online-assignments/module-5/index.html/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
